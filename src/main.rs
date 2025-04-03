fef91a9df6b199c49458585c2cf81bb75d369b24
Julian Schuler <julianschuler@users.noreply.github.com>
2025-04-03 22:26:49 +0200

    Extract TUI code into separate module

--- src/main.rs
+++ src/main.rs
@@ -1,106 +1,23 @@
-use std::io::{stdout, Result};
+mod chords;
+mod tui;
+
+use std::io::Result;
 
 use chords::Chords;
-use crossterm::{
-    event::{
-        read, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEvent, KeyModifiers,
-    },
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-};
-use tui::{
-    backend::{Backend, CrosstermBackend},
-    text::Text,
-    widgets::{Block, Borders, Paragraph},
-    Terminal,
-};
-
-mod chords;
+use tui::Tui;
 
 fn main() -> Result<()> {
-    enable_raw_mode()?;
-    let mut stdout = stdout();
-    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
+    const CHORDS_PATH: &str = "chords.txt";
 
-    let chords_path = "chords.txt";
-    let chords = Chords::read_from_file(chords_path)?;
+    let mut tui = Tui::new()?;
+    let chords = Chords::read_from_file(CHORDS_PATH)?;
 
-    if let Err(error) = run_event_loop(&mut terminal) {
-        eprintln!("Error when running main loop: {error}");
+    if let Err(error) = tui.run_event_loop() {
+        eprintln!("Error when running event loop: {error}");
     }
 
-    chords.write_to_file(chords_path)?;
-    disable_raw_mode()?;
-    execute!(
-        terminal.backend_mut(),
-        LeaveAlternateScreen,
-        DisableMouseCapture
-    )?;
+    chords.write_to_file(CHORDS_PATH)?;
+    tui.finish()?;
 
     Ok(())
-}
-
-fn run_event_loop<B: Backend>(terminal: &mut Terminal<B>) -> Result<()> {
-    let mut search_field = String::new();
-
-    loop {
-        draw_tui(terminal, &search_field)?;
-
-        let event = read()?;
-        if let Event::Key(key) = event {
-            if handle_key(key, &mut search_field) {
-                break;
-            }
-        }
-    }
-
-    Ok(())
-}
-
-fn draw_tui<B: Backend>(terminal: &mut Terminal<B>, search_field: &str) -> Result<()> {
-    terminal.draw(|frame| {
-        let text = Text::from(search_field);
-        let block = Block::default()
-            .borders(Borders::ALL)
-            .title("Search chords");
-        let paragraph = Paragraph::new(text).block(block);
-
-        frame.render_widget(paragraph, frame.size());
-    })?;
-
-    let x: u16 = search_field.len().try_into().unwrap();
-    terminal.set_cursor(x + 1, 1)?;
-    terminal.show_cursor()?;
-
-    Ok(())
-}
-
-fn handle_key(key: KeyEvent, search_field: &mut String) -> bool {
-    match key.code {
-        KeyCode::Char(char) => {
-            if key.modifiers.contains(KeyModifiers::CONTROL) {
-                match key.code {
-                    KeyCode::Char('c') => {
-                        // ctrl-c
-                        return true;
-                    }
-                    KeyCode::Char('h') => {
-                        // ctrl-backspace
-                        search_field.clear();
-                    }
-                    _ => {}
-                }
-            } else {
-                search_field.push(char);
-            }
-        }
-        KeyCode::Backspace => {
-            search_field.pop();
-        }
-        _ => {}
-    }
-
-    false
 }
---
+++ src/tui.rs
@@ -0,0 +1,109 @@
+use std::io::{stdout, Result, Stdout};
+
+use crossterm::{
+    event::{
+        read, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEvent, KeyModifiers,
+    },
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use tui::{
+    backend::CrosstermBackend,
+    text::Text,
+    widgets::{Block, Borders, Paragraph},
+    Terminal,
+};
+
+pub struct Tui {
+    terminal: Terminal<CrosstermBackend<Stdout>>,
+    search_field: String,
+}
+
+impl Tui {
+    pub fn new() -> Result<Self> {
+        enable_raw_mode()?;
+
+        let mut stdout = stdout();
+        execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+
+        let backend = CrosstermBackend::new(stdout);
+        let terminal = Terminal::new(backend)?;
+
+        Ok(Self {
+            terminal,
+            search_field: String::new(),
+        })
+    }
+
+    pub fn finish(mut self) -> Result<()> {
+        disable_raw_mode()?;
+        execute!(
+            self.terminal.backend_mut(),
+            LeaveAlternateScreen,
+            DisableMouseCapture
+        )?;
+
+        Ok(())
+    }
+
+    pub fn run_event_loop(&mut self) -> Result<()> {
+        loop {
+            self.draw()?;
+
+            let event = read()?;
+            if let Event::Key(key) = event {
+                if self.handle_key(key) {
+                    break;
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn draw(&mut self) -> Result<()> {
+        self.terminal.draw(|frame| {
+            let text = Text::from(self.search_field.as_str());
+            let block = Block::default()
+                .borders(Borders::ALL)
+                .title("Search chords");
+            let paragraph = Paragraph::new(text).block(block);
+
+            frame.render_widget(paragraph, frame.size());
+        })?;
+
+        let x: u16 = self.search_field.len().try_into().unwrap_or(u16::MAX - 1);
+        self.terminal.set_cursor(x + 1, 1)?;
+        self.terminal.show_cursor()?;
+
+        Ok(())
+    }
+
+    pub fn handle_key(&mut self, key: KeyEvent) -> bool {
+        match key.code {
+            KeyCode::Char(char) => {
+                if key.modifiers.contains(KeyModifiers::CONTROL) {
+                    match key.code {
+                        KeyCode::Char('c') => {
+                            // ctrl-c
+                            return true;
+                        }
+                        KeyCode::Char('h') => {
+                            // ctrl-backspace
+                            self.search_field.clear();
+                        }
+                        _ => {}
+                    }
+                } else {
+                    self.search_field.push(char);
+                }
+            }
+            KeyCode::Backspace => {
+                self.search_field.pop();
+            }
+            _ => {}
+        }
+
+        false
+    }
+}
