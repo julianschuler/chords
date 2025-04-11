ee5b0f214b1e21cf41c4dfca00b25dd6807a64ce
Julian Schuler <julianschuler@users.noreply.github.com>
2025-04-11 16:31:15 +0200

    Make paths to chord file and word list configurable

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,36 @@ mod chords;
 mod tui;
 mod words;
 
-use std::io::Result;
+use std::{io::Result, path::PathBuf};
 
 use chords::Chords;
+use clap::Parser;
 use tui::Tui;
 use words::Words;
 
-fn main() -> Result<()> {
-    const CHORDS_PATH: &str = "chords.txt";
-    const WORDS_PATH: &str = "words.txt";
+/// Small TUI program to manage a library of chords for QMK
+#[derive(Parser)]
+struct Args {
+    /// Path of the file to read/write all chords from/to
+    #[arg(short, long)]
+    chords: PathBuf,
+    /// Path to a list of common words sorted by frequeny in descending order
+    #[arg(short, long)]
+    words: PathBuf,
+}
 
-    let mut chords = Chords::read_from_file(CHORDS_PATH)?;
-    let words = Words::read_from_file_and_chords(WORDS_PATH, &chords)?;
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let mut chords = Chords::read_from_file(&args.chords)?;
+    let words = Words::read_from_file_and_chords(args.words, &chords)?;
     let mut tui = Tui::new(words)?;
 
     if let Err(error) = tui.run_event_loop(&mut chords) {
         eprintln!("Error when running event loop: {error}");
     }
 
-    chords.write_to_file(CHORDS_PATH)?;
+    chords.write_to_file(args.chords)?;
     tui.finish()?;
 
     Ok(())
